8846165dcde2773d0d628f9aac70566e5523b5a9
Ilma Salsabil <106610322+crocmons@users.noreply.github.com>
2023-02-04 08:50:10 +0600

    code

--- server/server.js
+++ server/server.js
@@ -92,7 +92,7 @@ app.post("/forget-password",async(req,res)=>{
         const token = jwt.sign({email:existingUser.email, id:existingUser._id},secret,{expiresIn:"5m"});
         const link = `https://mern-authentication-api-16k2.onrender.com/reset-password/${existingUser._id}/${token}`;
        
-         const email = req.body.email;
+//          const email = req.body.email;
 //          const subject = req.body.subject;
 //          const message = req.body.message;
 
